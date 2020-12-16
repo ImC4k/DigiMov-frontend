3d633bbc847473736e00b4daf4299acf071e9775
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-16 16:19:37 +0800

    [# 75 searchCinemasByCinemaName] feat: add search function in cinemaListPage to allow search cinema by keyword

--- src/Component/CinemaListPage/index.jsx
+++ src/Component/CinemaListPage/index.jsx
@@ -4,20 +4,35 @@ import { getAllCinemas } from '../../apis/cinema';
 import './CinemaListPage.css';
 import { Grid } from '@material-ui/core';
 class index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { keyword: "" };
+  }
   componentDidMount() {
     getAllCinemas().then((response) => {
       this.props.initCinemaList(response.data);
     });
   }
+  changeKeyWord = (event) => {
+    this.setState({ keyword: event.target.value })
+  }
   render() {
+    const keyword = this.state.keyword;
+    var filteredCinemaList = this.props.cinemaList;
+    if (keyword !== "") {
+      filteredCinemaList = filteredCinemaList.filter(cinema => 
+        cinema.name.toLowerCase().includes(keyword.toLowerCase())
+      )
+    }
+
     const cinemas =
-      this.props.cinemaList.length > 0 ? (
-        this.props.cinemaList.map((cinema) => (
-          <CinemaCard key={cinema.id} cinema={cinema} />
-        ))
+      filteredCinemaList.length > 0 ? (
+        filteredCinemaList.map((cinema) => {
+          return <CinemaCard key={cinema.id} cinema={cinema} />
+        })
       ) : (
-        <p className={'indicator-text'}>No available cinema</p>
-      );
+          <p className={'indicator-text'}>No available cinema</p>
+        );
     return (
       <Grid container justify='center' alignItems='center'>
         <Grid container item xs={10} className={'paper-content'}>
@@ -25,7 +40,7 @@ class index extends Component {
             <div className={'section-title'}>Cinemas</div>
           </Grid>
           <Grid container item xs={12} >
-            <input type='text' className={'search-box'} placeholder='Search' />
+            <input type='text' className={'search-box'} placeholder='Search' onChange={this.changeKeyWord} />
             {cinemas}
           </Grid>
         </Grid>
