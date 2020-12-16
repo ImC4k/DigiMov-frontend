59ee3aa6dbad4d5889d90c0d41e5327aa1e8b60c
Alex Liu <liual@oocl.com>
2020-12-16 16:08:39 +0800

    [#70 PaymentResult][Alex Liu] feat: used state to get values

--- src/Component/PaymentResultPage/index.jsx
+++ src/Component/PaymentResultPage/index.jsx
@@ -2,8 +2,57 @@ import "./PaymentResultPage.css";
 import React, { Component } from "react";
 import { Grid } from "@material-ui/core";
 import QRCode from "qrcode.react";
+import moment from 'moment';
 
 export default class index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {order: {
+            id: 123,
+            email: "a@a.com",
+            userId: 123,
+            bookedSeatIndices: [1,2],
+            customerGroupQuantityMap: {
+                "Adult": 1,
+                "Children": 1
+            },
+            movieSession: {
+                id: '5fd760d56500c940f174f4b0',
+                movie: {
+                    id: '5fd760d56500c940f174f4a0',
+                    name: 'Beyond the dream',
+                    duration: 100,
+                    genres: [
+                        {
+                            id: '5fd760d56500c940f174f4a0',
+                            name: 'Romance',
+                        },
+                    ],
+                    director: 'Kiwi Chow',
+                    description: 'I go to school by bus',
+                    imageUrl:
+                        'https://wmoov.com/assets/movie/photo/201912/FB_IMG_1576452551183_1576574597.jpg',
+                    rating: 'IIA',
+                    cast: ['ME', 'Them'],
+                    language: 'Cantonese',
+                },
+                house: {
+                    id: '5fd760d56500c940f174f4a01',
+                    cinemaId: '5fd760d56500c940f174f4b2',
+                    name: 'House 1',
+                    capacity: 100,
+                },
+                startTime: 1608018165676,
+                prices: {
+                    Adult: 100,
+                    Student: 60,
+                },
+                occupied: {},
+                occupancyCount: 1,
+            },
+            creditCardNumber: "1234",
+        }};
+    }
   render() {
     return (
       <div>
@@ -16,31 +65,31 @@ export default class index extends Component {
               <div className={"sub-section-title"}>Reference No.</div>
             </Grid>
             <Grid container item xs={12}>
-              112498823573592
+              {this.state.order.id}
             </Grid>
             <Grid container item xs={12}>
               <div className={"sub-section-title"}>Movie</div>
             </Grid>
             <Grid container item xs={12}>
-              movieSess 
+              {this.state.order.movieSession.movie.name}
             </Grid>
             <Grid container item xs={12}>
               <div className={"sub-section-title"}>Session</div>
             </Grid>
             <Grid container item xs={12}>
-              2020/9/1 21:30
+             {moment(this.state.order.movieSession.startTime).format('YYYY/MM/DD HH:mm').toString()}
             </Grid>
             <Grid container item xs={12}>
               <div className={"sub-section-title"}>House</div>
             </Grid>
             <Grid container item xs={12}>
-              House 1
+              {this.state.order.movieSession.house.name}
             </Grid>
             <Grid container item xs={12}>
               <div className={"sub-section-title"}>Seats</div>
             </Grid>
             <Grid container item xs={12}>
-              F5, F6
+              {this.state.order.bookedSeatIndices.join(', ')}
             </Grid>
             <Grid container item xs={12}>
               <div className={"sub-section-title"}>Digi-ticket</div>
