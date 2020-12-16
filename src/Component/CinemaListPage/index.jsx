cecae3ce4c6bf301d71d3196e2dc1ff3f5bacc8b
Six Eggs <sixeggs0303@gmail.com>
2020-12-16 21:13:08 +0800

    [#73StylingCinemaPickerPage][Marcus] feat: added loading animation for cinema list.

--- src/Component/CinemaListPage/index.jsx
+++ src/Component/CinemaListPage/index.jsx
@@ -4,14 +4,16 @@ import { getAllCinemas } from '../../apis/cinema';
 import '../Style/commonStyle.css';
 import './CinemaListPage.css';
 import { Grid } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 class index extends Component {
   constructor(props) {
     super(props)
-    this.state = { keyword: "" };
+    this.state = { keyword: "", loadingData: true };
   }
   componentDidMount() {
     getAllCinemas().then((response) => {
       this.props.initCinemaList(response.data);
+      this.setState({loadingData: false});
     });
   }
   changeKeyWord = (event) => {
@@ -27,13 +29,12 @@ class index extends Component {
     }
 
     const cinemas =
-      filteredCinemaList.length > 0 ? (
-        filteredCinemaList.map((cinema) => {
-          return <CinemaCard key={cinema.id} cinema={cinema} />
-        })
-      ) : (
-          <p className={'indicator-text'}>No available cinema</p>
-        );
+      filteredCinemaList.length > 0 ? 
+        (filteredCinemaList.map((cinema) => { return <CinemaCard key={cinema.id} cinema={cinema} /> })) : 
+        (this.state.loadingData ? 
+          (<Grid container item xs={12} justify='center'><CircularProgress className={'loading-cirle'}/></Grid>) : 
+          (<p className={'indicator-text'}>No available cinema</p>));
+
     return (
       <Grid container justify='center' alignItems='center'>
         <Grid container item xs={10} className={'main-content'}>
