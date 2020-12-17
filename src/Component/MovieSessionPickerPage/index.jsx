e241d03433f7a06d81b4d63960af8c20f0905f7a
Six Eggs <sixeggs0303@gmail.com>
2020-12-17 21:57:56 +0800

    [#86DevUIAlignment][Marcus] fix: session dropdown margin. fail status shown before finishing loading

--- src/Component/MovieSessionPickerPage/index.jsx
+++ src/Component/MovieSessionPickerPage/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { Divider, Grid, CircularProgress, Modal } from '@material-ui/core';
+import { Divider, Grid, Modal } from '@material-ui/core';
 import '../Style/commonStyle.css';
 
 import MovieInfoModal from '../MovieInfoModal';
 
 import { getMovie } from '../../apis/movie';
 import { getUpcomingMovieSessionListByMovieId } from '../../apis/movieSession';
+import CircularLoading from '../Style/CircularLoading';
 
 
 import MovieSessionListContainer from '../../Container/MovieSessionPickerPageContainer/MovieSessionListContainer'
@@ -70,25 +71,18 @@ function MovieSessionPicker() {
                 <Grid container item xs={12}>
                     <div className={'section-header'}>Sessions</div>
                 </Grid>
-                {movie.id === undefined ?(
-                    loadingData ? 
-                    (<Grid container item xs={12} justify="center"><CircularProgress className={'loading-cirle'}/></Grid>) : 
-                    (<Grid container item xs={12}>Disconnected from server</Grid>)
-                ):
+
+                { loadingData ? <Grid container item xs={12} justify="center"><CircularLoading /></Grid> : 
+
+                    movie.id === undefined ? <Grid container item xs={12}>Disconnected from server</Grid> : 
+
                     <Grid container item xs={12}>
                         <Grid item xs={9}><div className={'section-sub-header'}>{movie.name}</div></Grid>
-                        <Grid container item xs={3} onClick={() => handleOpen(movie)} className={'link-style-button'} justify='flex-end' alignItems='center'>
-                            More details
-                        </Grid>
-                        <Grid item xs={12}><Divider className='margin-divider'/></Grid>
-                        {!loadingData && (movieSessions.length === 0 ? 
-                        loadingData ? 
-                        (<Grid container item xs={12} justify="center"><CircularProgress className={'loading-cirle'}/></Grid>) : 
-                        (<div>No available sessions</div>): 
-                        <Grid item xs={12}><MovieSessionListContainer movieSessions= {movieSessions}/></Grid>
-                        )}
+                        <Grid container item xs={3} onClick={() => handleOpen(movie)} className={'link-style-button'} justify='flex-end' alignItems='center'>More details</Grid>
+                    <Grid item xs={12}><Divider className='margin-divider'/></Grid>
 
-
+                    { movieSessions.length === 0 ? <div>No available sessions</div> : 
+                    <Grid item xs={12}><MovieSessionListContainer movieSessions= {movieSessions}/></Grid> }
                         <Modal open={open} onClose={handleClose}>
                             <div>
                             <MovieInfoModal movie={movieInfoInModal} />
