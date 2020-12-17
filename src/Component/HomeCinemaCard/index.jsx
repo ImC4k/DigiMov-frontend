4166485eb53ef4250bdffad5c118f1ae9b43a5c5
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-17 12:04:51 +0800

    [#71 HomePage][Ernest] Feat: Added redirect when click on home cinema card

--- src/Component/HomeCinemaCard/index.jsx
+++ src/Component/HomeCinemaCard/index.jsx
@@ -10,6 +10,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import { ERROR_IMAGE } from '../../assets';
 import './HomeCinemaCard.css';
 import { Grid } from "@material-ui/core";
+import { Redirect } from 'react-router-dom';
 
 class HomeCinemaCard extends Component {
     constructor(props) {
@@ -20,31 +21,46 @@ class HomeCinemaCard extends Component {
         };
     }
 
+    toggleCinemaCard() {
+        this.setState({ shouldRedirect: true });
+    }
+
+    redirectToCinemaSession(cinemaId) {
+        return <Redirect to={'/cinemas/' + cinemaId}></Redirect>;
+    }
+
     render() {
-        return (
-            <Grid container item xs={12}>
-                <Grid className={'card'} container item xs={12}>
-                    <Card
-                        className={'custom-card'}
-                    >
-                        <CardActionArea>
-                            <CardMedia
-                                className={'card-image'}
-                                src={this.state.imageUrl}
-                                component='img'
-                                onError={(e) => {
-                                    e.target.onError = null;
-                                    this.setImage(ERROR_IMAGE);
-                                }}>
-                            </CardMedia>
-                            <div className={'overlay'}>
-                                {this.props.cinema.name}
-                            </div>
-                        </CardActionArea>
-                    </Card>
+        const { shouldRedirect } = this.state;
+        const { name, address, id } = this.props.cinema;
+        if (shouldRedirect) {
+            return this.redirectToCinemaSession(id);
+        } else {
+            return (
+                <Grid container item xs={12}>
+                    <Grid className={'card'} container item xs={12}>
+                        <Card
+                            className={'custom-card'}
+                            onClick={() => this.toggleCinemaCard()}
+                        >
+                            <CardActionArea>
+                                <CardMedia
+                                    className={'card-image'}
+                                    src={this.state.imageUrl}
+                                    component='img'
+                                    onError={(e) => {
+                                        e.target.onError = null;
+                                        this.setImage(ERROR_IMAGE);
+                                    }}>
+                                </CardMedia>
+                                <div className={'overlay'}>
+                                    {this.props.cinema.name}
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-        )
+            )
+        }
     }
 }
 
